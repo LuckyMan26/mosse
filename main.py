bcfa4dfe1ae18fa70a46dd9973c52111244dd02f
LuckyMan26 <volikartem6@gmail.com>
2024-01-12 12:45:19 +0200

    Started implementing Mossetracker(created method for applying mask to the image)

--- main.py
+++ main.py
@@ -5,22 +5,44 @@
 
 import cv2
 import numpy as np
+from torchvision import transforms
+import torch
+
 def normalize_image(image):
     image_res =  image.astype(np.float32)
     image_res = np.sign(image_res - 127)*np.sqrt(np,abs(image_res - 127))
     # normalization
-    image_res -= np.mean(image)
-    image /= np.linalg.norm(image)
-    return image
+    normalize = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    normalized_image = normalize(image)
+    return normalized_image
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+
+
+
+class MOSSETracker(object):
+    def __init__(self, F):
+        transform = transforms.ToTensor()
+        F_tensor = transform(F)
+        self.F = F_tensor
+        self.P_transformation = 8
+        self.learning_rate = 0.25
+    def apply_mask(self, mask):
+        self.F  =  self.F * mask.unsqueeze(0)
+    def to_fft(self):
+        self.F_fft = torch.fft.fft2(F)
+
+    def train(image, eta_current):
+        counter = 0
+        while counter < self.P_transformation:
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

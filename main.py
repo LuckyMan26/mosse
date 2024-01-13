3844f67760d78453c739f6906597c6d96bfbf809
LuckyMan26 <volikartem6@gmail.com>
2024-01-13 12:21:25 +0200

    Working version(hope so)

--- main.py
+++ main.py
@@ -13,22 +13,11 @@ from PIL import Image
 def normalize_image(image):
     res = torch.tensor(image, dtype=torch.float32)
 
-    res = torch.sign(res - 127.5) * torch.sqrt(torch.abs(res - 127.5))
+    res = torch.sign(res - 127) * torch.sqrt(torch.abs(res - 127))
     res -= torch.mean(res)
     res /= torch.norm(res)
 
     return res
-
-
-def random_affine_transform(image, center_point, scale_factor_range=(0.9, 1.1), rotation_range=(-10, 10)):
-    print('Center', center_point)
-    random_affine_transform = transforms.RandomAffine(degrees=(-3, 3), translate=(0.1, 0.3), scale=(0.9, 1.1), center=center_point)
-
-    transformed_image = random_affine_transform(image)
-
-    transformed_tensor = transforms.ToTensor()(transformed_image)
-
-    return transformed_tensor
 
 
 def hanning_window_2d(height, width):
@@ -44,6 +33,37 @@ def normalize(array):
     maximum = torch.max(array)
     minimum = torch.min(array)
     return (array - minimum) / (maximum - minimum)
+def get_random_transform_mat(roi):
+    amplitude = 0.025
+    min_angle, max_angle = -3, 3
+    angle = np.random.uniform(min_angle, max_angle)
+    origin_pts = np.zeros(shape=(3, 2), dtype=np.float32)
+
+    origin_pts[0, 0] = roi[0]
+    origin_pts[0, 1] = roi[1]
+    origin_pts[1, 0] = roi[0] + roi[2]
+    origin_pts[1, 1] = roi[1]
+    origin_pts[2, 0] = roi[0]
+    origin_pts[2, 1] = roi[1] + roi[3]
+
+    center_x = roi[0] + roi[2] / 2
+    center_y = roi[1] + roi[3] / 2
+    rotation_matrix = cv2.getRotationMatrix2D((center_x, center_y), angle, 1)
+    extended_rotation_matrix = np.concatenate((rotation_matrix, np.asarray([[0.0, 0.0, 1.0]])))
+
+    target_pts = np.copy(origin_pts)
+    affine_noise = np.asarray(np.random.uniform(-amplitude, amplitude, size=(3, 2)), dtype=np.float32)
+    origin_pts[0, 0] += roi[2] * affine_noise[0, 0]
+    origin_pts[0, 1] += roi[3] * affine_noise[0, 1]
+    origin_pts[1, 0] += roi[2] * affine_noise[1, 0]
+    origin_pts[1, 1] += roi[3] * affine_noise[1, 1]
+    origin_pts[2, 0] += roi[2] * affine_noise[2, 0]
+    origin_pts[2, 1] += roi[3] * affine_noise[2, 1]
+    affine_matrix = cv2.getAffineTransform(origin_pts, target_pts)
+    extended_affine_matrix = np.concatenate((affine_matrix, np.asarray([[0.0, 0.0, 1.0]])))
+    full_transform = np.matmul(extended_rotation_matrix, extended_affine_matrix)
+
+    return full_transform[0:2]
 
 class MOSSETracker(object):
     def __init__(self, roi, learning_rate, transforms_number, psr_thr, sigma):
@@ -53,8 +73,8 @@ class MOSSETracker(object):
         self.W_fft = None
         self.g_max = None
 
-        self.F_fft = None
-        self.F = None
+
+
         self.G = None
         self.P_transformation = transforms_number
         self.learning_rate = learning_rate
@@ -68,16 +88,11 @@ class MOSSETracker(object):
         self.W = None
         self.PSR_thr = psr_thr
 
-    def apply_mask(self, mask):
-        self.F = mask*self.F + (1 - mask)*torch.mean(self.F)
 
-    def to_fft(self):
-        self.F_fft = torch.fft.fft2(self.F)
-        self.G_fft = torch.fft.fft2(self.G)
 
     def desired_response(self):
         xx, yy = np.meshgrid(np.arange(self.roi[2]), np.arange(self.roi[3]))
-        center_x, center_y = self.roi[2] / 2, self.roi[3] / 2
+        center_x, center_y = ( self.roi[2]) / 2,(self.roi[3]) / 2
         sigma = self.sigma_amplitude * np.sqrt(self.roi[2]*self.roi[3])
         distance_map = (np.square(xx - center_x) + np.square(yy - center_y)) / (sigma * sigma)
         response = np.exp(-distance_map)
@@ -100,23 +115,28 @@ class MOSSETracker(object):
         B_new = F_object_image_freq * (F_object_image_freq)
 
         while counter < self.P_transformation:
-            center = [self.roi[0] + self.roi[2] / 2, self.roi[1] + self.roi[3] / 2]
-            tensor_to_pil = transforms.ToPILImage()(image)
 
-            affine_transform = random_affine_transform(tensor_to_pil, center)
 
-            affine_transform = affine_transform.squeeze()
-            F = affine_transform[self.roi[1]:self.roi[1] + self.roi[3], self.roi[0]:self.roi[0] + self.roi[2]]
+
+            affine_transform_matrix = get_random_transform_mat(self.roi)
+            transformed_image = cv2.warpAffine(image.numpy(), affine_transform_matrix, (image.shape[1], image.shape[0]))
+            transformed_image = transformed_image[self.roi[1]:self.roi[1]+self.roi[3], self.roi[0]:self.roi[0]+self.roi[2]]
+
+            transform = transforms.ToTensor()
+            transformed_image.squeeze()
+            F = transform(transformed_image)
+            F = F.squeeze()
+
 
             mask = hanning_window_2d(F.shape[0], F.shape[1])
 
             F = mask * F + (1 - mask) * torch.mean(F)
-            self.F = F
 
-            self.F_fft = torch.fft.fft2(self.F)
 
-            A_new += self.G_fft * torch.conj(self.F_fft)
-            B_new += self.F_fft * torch.conj(self.F_fft)
+            F_fft = torch.fft.fft2(F)
+
+            A_new += self.G_fft * torch.conj(F_fft)
+            B_new += F_fft * torch.conj(F_fft)
 
             if learning_rate >= 1.0:
                 self.A = A_new
@@ -136,6 +156,7 @@ class MOSSETracker(object):
         self.G = self.desired_response()
 
         self.G_fft = torch.fft.fft2(self.G)
+
         self.train(self.image, 1.0)
 
 
@@ -143,29 +164,27 @@ class MOSSETracker(object):
 
         ok = True
         frame = normalize_image(frame)
-        transform = transforms.ToTensor()
+
         prev_object_neighbourhood = frame[self.roi[1]:self.roi[1] + self.roi[3],
                                     self.roi[0]:self.roi[0] + self.roi[2]]
 
+        F = prev_object_neighbourhood
+        mask = hanning_window_2d(F.shape[0], F.shape[1])
 
-        self.F = prev_object_neighbourhood
-        mask = hanning_window_2d(self.F.shape[0], self.F.shape[1])
+        F = mask * F + (1 - mask) * torch.mean(F).item()
 
-        self.apply_mask(mask)
+        F_fft = torch.fft.fft2(F)
 
-        self.F_fft = torch.fft.fft2(self.F)
+        G_response_fft = self.W_fft * F_fft
 
-        G_response_fft = self.W_fft * self.F_fft
-
-        G_response = torch.abs(torch.fft.ifft(G_response_fft))
+        G_response = torch.abs(torch.fft.ifft2(G_response_fft))
 
         max_response = G_response.max()
 
         max_position = np.where(G_response == max_response)
         x_max_position = int(np.mean(max_position[1]))
         y_max_position = int(np.mean(max_position[0]))
-        print('Position', x_max_position, y_max_position)
-        print(G_response.shape)
+
         dx = x_max_position - G_response.shape[1] // 2
         dy = y_max_position - G_response.shape[0] // 2
 
@@ -174,7 +193,7 @@ class MOSSETracker(object):
         g_sidelobes[y_max_position, x_max_position] = 0.0
         mean_sidelobes = np.mean(g_sidelobes)
         std_sidelobes = np.std(g_sidelobes)
-        print("Delta ", dx, dy)
+
         PSR = (max_response - std_sidelobes) / (mean_sidelobes + 0.0001)
         if PSR >= self.PSR_thr:
             ok = True
@@ -192,7 +211,7 @@ class MOSSETracker(object):
             elif self.roi[1] >= self.image.shape[0] - self.roi[3]:
                 self.roi[1] = self.image.shape[0] - self.roi[3] - 1
 
-            self.train(self.image, self.learning_rate)
+            self.train(frame, self.learning_rate)
         else:
             ok = False
         return self.roi, ok
@@ -238,7 +257,3 @@ if __name__ == '__main__':
 
 
     out.release()
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

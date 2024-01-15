c15c4d36847896b63302b4d8d8c5b9456d9eec21
LuckyMan26 <volikartem6@gmail.com>
2024-01-15 14:43:45 +0200

    Mosse tracker implementation

--- main.py
+++ main.py
@@ -66,13 +66,15 @@ def get_random_transform_mat(roi):
     return full_transform[0:2]
 
 class MOSSETracker(object):
-    def __init__(self, roi, learning_rate, transforms_number, psr_thr, sigma):
+    def __init__(self, roi, learning_rate, transforms_number, psr_thr, sigma, scaling_factor):
 
         self.image = None
         self.G_fft = None
         self.W_fft = None
+        self.W_fft_incr = None
+        self.W_fft_decr = None
         self.g_max = None
-
+        self.scaling_factor = scaling_factor
 
 
         self.G = None
@@ -85,6 +87,10 @@ class MOSSETracker(object):
         self.sigma_amplitude = sigma
         self.A = None
         self.B = None
+        self.A_incr = None
+        self.B_incr = None
+        self.A_decr = None
+        self.B_decr = None
         self.W = None
         self.PSR_thr = psr_thr
 
@@ -101,7 +107,8 @@ class MOSSETracker(object):
         return response
 
 
-    def train(self, image, learning_rate):
+    def train(self,image, W_fft,A,B, learning_rate):
+
         counter = 0
         A_new = 0
         B_new = 0
@@ -109,6 +116,7 @@ class MOSSETracker(object):
         f_object_image = image[self.roi[1]:self.roi[1]+self.roi[3], self.roi[0]:self.roi[0]+self.roi[2]]
         mask = hanning_window_2d(f_object_image.shape[0], f_object_image.shape[1])
         f_object_image = mask * f_object_image + (1 - mask) * torch.mean(f_object_image)
+
         F_object_image_freq = torch.fft.fft2(f_object_image)
 
         A_new = self.G_fft * (F_object_image_freq)
@@ -116,9 +124,8 @@ class MOSSETracker(object):
 
         while counter < self.P_transformation:
 
-
-
             affine_transform_matrix = get_random_transform_mat(self.roi)
+
             transformed_image = cv2.warpAffine(image.numpy(), affine_transform_matrix, (image.shape[1], image.shape[0]))
             transformed_image = transformed_image[self.roi[1]:self.roi[1]+self.roi[3], self.roi[0]:self.roi[0]+self.roi[2]]
 
@@ -139,31 +146,54 @@ class MOSSETracker(object):
             B_new += F_fft * torch.conj(F_fft)
 
             if learning_rate >= 1.0:
-                self.A = A_new
-                self.B = B_new
+                A = A_new
+                B = B_new
 
             else:
-                self.A = learning_rate * A_new + (1 - learning_rate) * self.A
-                self.B = learning_rate * B_new + (1 - learning_rate) * self.B
+                A = learning_rate * A_new + (1 - learning_rate) * A
+                B = learning_rate * B_new + (1 - learning_rate) * B
 
-            self.W_fft = torch.divide(self.A, self.B + 0.0001)
+            W_fft = torch.divide(A, B + 0.0001)
             counter += 1
+        return W_fft, A,B
 
     def initialize(self, image, roi):
         self.roi = roi
+        new_width_incr = int(image.shape[1] * (1 + self.scaling_factor))
+        new_height_incr = int(image.shape[0] * (1 + self.scaling_factor))
+        incr_image = cv2.resize(image, (new_width_incr, new_height_incr))
+        new_width_decr = int(image.shape[1] * (1 - self.scaling_factor))
+        new_height_decr = int(image.shape[0] * (1 - self.scaling_factor))
+        decr_image = cv2.resize(image, (new_width_decr, new_height_decr))
 
         self.image = normalize_image(image)
+        incr_image = normalize_image(incr_image)
+        decr_image = normalize_image(decr_image)
         self.G = self.desired_response()
 
         self.G_fft = torch.fft.fft2(self.G)
 
-        self.train(self.image, 1.0)
+        self.W_fft,self.A, self.B = self.train(self.image, self.W_fft, self.A, self.B, 1.0)
+        self.W_fft_incr, self.A_incr, self.B_incr = self.train(incr_image, self.W_fft_incr, self.A_incr, self.B_incr, 1.0)
+        self.W_fft_decr, self.A_decr, self.B_decr = self.train(decr_image, self.W_fft_decr, self.A_decr, self.B_decr, 1.0)
 
 
     def tracking(self, frame):
 
         ok = True
+        new_width_incr = int(frame.shape[1] * (1 + self.scaling_factor))
+        new_height_incr = int(frame.shape[0] * (1 + self.scaling_factor))
+        incr_image = cv2.resize(frame, (new_width_incr, new_height_incr))
+        new_width_decr = int(frame.shape[1] * (1 - self.scaling_factor))
+        new_height_decr = int(frame.shape[0] * (1 - self.scaling_factor))
+        decr_image = cv2.resize(frame, (new_width_decr, new_height_decr))
+
         frame = normalize_image(frame)
+        incr_image = normalize_image(incr_image)
+        decr_image = normalize_image(decr_image)
+
+
+
 
         prev_object_neighbourhood = frame[self.roi[1]:self.roi[1] + self.roi[3],
                                     self.roi[0]:self.roi[0] + self.roi[2]]
@@ -176,19 +206,44 @@ class MOSSETracker(object):
         F_fft = torch.fft.fft2(F)
 
         G_response_fft = self.W_fft * F_fft
+        G_response_fft_incr = self.W_fft_incr * F_fft
+        G_response_fft_decr = self.W_fft_decr * F_fft
 
         G_response = torch.abs(torch.fft.ifft2(G_response_fft))
+        G_response_decr = torch.abs(torch.fft.ifft2(G_response_fft_decr))
+        G_response_incr = torch.abs(torch.fft.ifft2(G_response_fft_incr))
 
         max_response = G_response.max()
+        max_response_decr = G_response_decr.max()
+        max_response_incr = G_response_incr.max()
+        g_sidelobes = None
+        if max_response >= max_response_decr and max_response >= max_response_incr:
+            max_position = np.where(G_response == max_response)
+            x_max_position = int(np.mean(max_position[1]))
+            y_max_position = int(np.mean(max_position[0]))
 
-        max_position = np.where(G_response == max_response)
-        x_max_position = int(np.mean(max_position[1]))
-        y_max_position = int(np.mean(max_position[0]))
+            dx = x_max_position - G_response.shape[1] // 2
+            dy = y_max_position - G_response.shape[0] // 2
+            g_sidelobes = (G_response - self.G).numpy()
+        elif max_response_decr >= max_response and max_response_decr >= max_response_incr:
+            max_position = np.where(G_response_decr == max_response_decr)
+            x_max_position = int(np.mean(max_position[1]))
+            y_max_position = int(np.mean(max_position[0]))
 
-        dx = x_max_position - G_response.shape[1] // 2
-        dy = y_max_position - G_response.shape[0] // 2
+            dx = x_max_position - G_response_decr.shape[1] // 2
+            dy = y_max_position - G_response_decr.shape[0] // 2
+            g_sidelobes = (G_response_decr - self.G).numpy()
 
-        g_sidelobes = (G_response - self.G).numpy()
+        elif max_response_incr >= max_response_decr and max_response_incr >= max_response:
+            max_position = np.where(G_response_incr == max_response_incr)
+            x_max_position = int(np.mean(max_position[1]))
+            y_max_position = int(np.mean(max_position[0]))
+
+            dx = x_max_position - G_response_incr.shape[1] // 2
+            dy = y_max_position - G_response_incr.shape[0] // 2
+            g_sidelobes = (G_response_incr - self.G).numpy()
+
+
         g_sidelobes[g_sidelobes < 0] = 0.0
         g_sidelobes[y_max_position, x_max_position] = 0.0
         mean_sidelobes = np.mean(g_sidelobes)
@@ -211,12 +266,19 @@ class MOSSETracker(object):
             elif self.roi[1] >= self.image.shape[0] - self.roi[3]:
                 self.roi[1] = self.image.shape[0] - self.roi[3] - 1
 
-            self.train(frame, self.learning_rate)
+            self.W_fft, self.A, self.B = self.train(frame, self.W_fft,self.A, self.B,  self.learning_rate)
+            self.W_fft_incr, self.A_incr, self.B_incr = self.train(decr_image, self.W_fft_decr,self.A_decr,self.B_decr,  self.learning_rate)
+            self.W_fft_incr, self.A_incr, self.B_incr = self.train(incr_image, self.W_fft_incr,self.A_incr, self.B_incr, self.learning_rate)
         else:
             ok = False
+
         return self.roi, ok
 
-
+def read_photo(i):
+    filename = f"{i:05d}.jpg"
+    path = "E:\\Mohiniyattam\\img\\" + filename
+    frame = cv2.imread(path)
+    return frame
 if __name__ == '__main__':
 
     video_path = 'E:\\plane.mp4'
@@ -225,14 +287,14 @@ if __name__ == '__main__':
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = cap.get(cv2.CAP_PROP_FPS)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    output_file = 'E:\\video2.mp4'
+    output_file = 'E:\\plane_tracker.mp4'
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_file, fourcc, fps, (width, height), True)
     if not cap.isOpened():
         print("Error: Could not open video file.")
         exit()
 
-    ret,frame = cap.read()
+    ret, frame = cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     tracked_object_roi = list(cv2.selectROI('frame', frame))
@@ -240,7 +302,7 @@ if __name__ == '__main__':
     tracker = MOSSETracker(roi=tracked_object_roi, learning_rate=0.2,
                            transforms_number=8,
                            psr_thr=5.7,
-                           sigma=0.025)
+                           sigma=0.025, scaling_factor=0.03)
     tracker.initialize(frame, tracked_object_roi)
     counter = 0
     while True:
@@ -254,6 +316,5 @@ if __name__ == '__main__':
         cv2.imshow("MOSSE tracker", frame)
         out.write(cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR))
         cv2.waitKey(1)
-
 
     out.release()
